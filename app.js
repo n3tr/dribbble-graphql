c27ffa9e8d25608b09624eec10ee3b567d08ba6a
Jirat Ki <saakyz@gmail.com>
2016-09-09 01:44:19 +0700

    Shots JSON Data and Shots Query

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-const graphql = require('graphql')
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLList } from 'graphql'
 
-const app = express()
 
-// Create GraphQL schrma
-const schema = new graphql.GraphQLSchema({
-  query: new graphql.GraphQLObjectType({
+// GraphQL ObjectType
+import Shot from './type/shot'
+
+// Static Data
+import shotsData from './data/shots.json'
+
+// Define the schema with one top-level field, `user`, that
+// takes an `id` argument and returns the User with that ID.
+// Note that the `query` is a GraphQLObjectType, just like User.
+// The `user` field, however, is a userType, which we defined above.
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
     name: 'Query',
     fields: {
-      hello: {
-        type: graphql.GraphQLString,
-        resolve: () => {
-          return "world"
-        }
+      shots: {
+        type: new GraphQLList(Shot),
+        resolve: () => shotsData
       }
     }
   })
 })
+
+const app = express()
 
 app.use('/graphql', graphqlHTTP({
   schema,
